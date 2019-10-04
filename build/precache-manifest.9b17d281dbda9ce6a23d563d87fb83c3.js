f7c83c1ea119801c8f56b11a81dfcc48249f12da
John B <john@nexys.ch>
2019-10-04 20:31:38 +0200

    build

---
+++ build/precache-manifest.9b17d281dbda9ce6a23d563d87fb83c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f1f73efd78bdf740f691510c99dc96",
+    "url": "Nexysweb/tabular-excel/build/index.html"
+  },
+  {
+    "revision": "ce288204cfa41a2c65bb",
+    "url": "Nexysweb/tabular-excel/build/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "08e28f548740123f1623",
+    "url": "Nexysweb/tabular-excel/build/static/js/2.12dcbf67.chunk.js"
+  },
+  {
+    "revision": "ce288204cfa41a2c65bb",
+    "url": "Nexysweb/tabular-excel/build/static/js/main.acb013c0.chunk.js"
+  },
+  {
+    "revision": "78965d31bb00f3313148",
+    "url": "Nexysweb/tabular-excel/build/static/js/runtime-main.ac4f0ef4.js"
+  }
+]);
