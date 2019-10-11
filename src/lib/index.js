3a5387096f3799a9b25f20e09f39f54a6ce1322a
John B <john@nexys.ch>
2019-10-11 17:33:08 +0200

    add index ref

---
+++ src/lib/index.js
@@ -0,0 +1,10 @@
+import * as LinesToRows from './lines-to-rows';
+import * as TabularXlsx from './tabular-xlsx';
+
+export {
+  LinesToRows, TabularXlsx
+}
+
+export default {
+  LinesToRows, TabularXlsx
+}
