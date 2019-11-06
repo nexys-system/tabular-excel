14d99207b3c4f0a6725eb047b44963dca01444d4
John B <john@nexys.ch>
2019-11-06 15:40:11 +0100

    new front

---
+++ build/precache-manifest.b6072aeae174bba1102836888253d7c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de3517dd5adbe99eb6342f5e663a227",
+    "url": "/tabular-excel/build/index.html"
+  },
+  {
+    "revision": "f7e5fb411b8142a04b39",
+    "url": "/tabular-excel/build/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bfae4dd9952b58c21b6f",
+    "url": "/tabular-excel/build/static/js/2.840e3511.chunk.js"
+  },
+  {
+    "revision": "f7e5fb411b8142a04b39",
+    "url": "/tabular-excel/build/static/js/main.7aa2780a.chunk.js"
+  },
+  {
+    "revision": "4cfd97741be249c778cf",
+    "url": "/tabular-excel/build/static/js/runtime-main.b0ab94a5.js"
+  }
+]);
