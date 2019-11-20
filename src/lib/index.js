4c1d81dff8a41028c24e2d73257bc8d722f04e4e
John B <john@nexys.ch>
2019-11-20 12:19:25 +0100

    fix index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,11 @@
 import * as LinesToRows from './lines-to-rows';
 import * as TabularXlsx from './tabular-xlsx';
+import * as Csv from './tabular-xlsx';
 
 export {
-  LinesToRows, TabularXlsx
+  LinesToRows, TabularXlsx, Csv
 }
 
 export default {
-  LinesToRows, TabularXlsx
+  LinesToRows, TabularXlsx, Csv
 }
