953e2a33659aaf63e863e4fa3c4a768667d71ea4
John B <john@nexys.ch>
2019-11-20 12:24:12 +0100

    added test for tsv

--- src/lib/csv.test.js
+++ src/lib/csv.test.js
@@ -31,3 +31,19 @@ test('csv with number', () => {
 
   expect(r).toEqual(e);
 });
+
+test('csv with number and delimited with tabs', () => {
+  const rows = [
+    ['a', 1],
+    ['c', 'd'],
+    ['e', 'f']
+  ];
+
+  const r = Csv.to(rows, '\t');
+
+  const e = `"a"\t1
+"c"\t"d"
+"e"\t"f"`;
+
+  expect(r).toEqual(e);
+});
