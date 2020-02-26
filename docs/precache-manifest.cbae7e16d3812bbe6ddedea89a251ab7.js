664afb3b0eec8cbd8dd068c074f41cbaa65c4966
Johan <johan@macair2020>
2020-02-26 15:00:08 +0100

    new GH Pages version

---
+++ docs/precache-manifest.cbae7e16d3812bbe6ddedea89a251ab7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014fd89b7c222f447c236ea3431fc17e",
+    "url": "/tabular-excel/index.html"
+  },
+  {
+    "revision": "64196f1ec202853bbbca",
+    "url": "/tabular-excel/static/js/2.fb0f166f.chunk.js"
+  },
+  {
+    "revision": "91d4e1651df48bb1dd0c",
+    "url": "/tabular-excel/static/js/main.afa353e3.chunk.js"
+  },
+  {
+    "revision": "3210ddfa0d292e36647d",
+    "url": "/tabular-excel/static/js/runtime-main.a36ff53b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tabular-excel/precache-manifest.01efbeb23ee11f4c1538185cb8221890.js"
+  "/tabular-excel/precache-manifest.cbae7e16d3812bbe6ddedea89a251ab7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tabular-excel/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
