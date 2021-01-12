c586699c164e7a022c92d004f157a6d8903de033
Johan <johan@macair2020>
2021-01-12 18:06:10 +0100

    removde old deploy script

--- sh/deploy.sh
+++
@@ -1,4 +0,0 @@
-yarn build
-git add -A
-git commit -a -m "new GH Pages version"
-git push origin master
--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -1,3 +1,11 @@
 import Input from "./input";
 
+/**
+ * <textarea
+  class="form-control"
+  placeholder="insert your json here"
+  style="min-width: 100%; height: 400px;"
+></textarea>;
+ */
+
 export default Input;
