db229e00e4e67cba5108a3d625830462592dc185
Johan <johan@macair2020>
2022-05-23 21:07:42 +0200

    buffer toBuffer

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./routes";
 
-import { Buffer as BufferPolyfill } from "Buffer";
-declare var Buffer: typeof BufferPolyfill;
-(globalThis as any).Buffer = BufferPolyfill;
-
-console.log("buffer", Buffer.from("foo", "hex"));
-
 ReactDOM.createRoot(document.getElementById("root")!).render(<Routes />);
