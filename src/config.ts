d9b6cced57e134d1049e4a3f6d71d8cc1bbe8bbb
John B <john@nexys.ch>
2022-09-26 18:19:02 +0200

    add footer + meta

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const ghUrl = "https://github.com/nexys-system/tabular-excel";
+
+export const version: string = import.meta.env.VITE_VERSION || "unset_version";
+export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
+export const basename = import.meta.env.BASE_URL;
+
+export const github = {
+  sha: `${ghUrl}/commit/${sha}`,
+  version: `${ghUrl}/releases/tag/${version}`,
+  url: ghUrl,
+};
